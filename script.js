cc66db98ad87ea25157368b94812d25c1d3fccec
Excellent Nomad <excellent.nomad@gmail.com>
2017-02-05 20:55:25 +0600

    Added comments

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var amount_taken = prompt("Kancha akcha berdi:");
 var amount_left = amount_taken - amount_due > 0 ? amount_taken - amount_due : 0;
 var output = "";
 
-
+// gets how many of each note cashier should return
 var getNofSpecificNote = function(amount, note){
   amount_left = amount%note;
   return {
@@ -15,10 +15,15 @@ var getNofSpecificNote = function(amount, note){
   };
 }
 
+// multiplies the notes array by 2, to make 0.5 => 1 so it
+// can be used in 'ways' function
 var newNotes = notes.map(function(value) {
   return value*2
 })
 
+// returns possible ways a cashier can return the change
+// reference: 
+//https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/CoinChanging.java
 var ways = function(amount_left, newNotes) {
   amount = amount_left * 2
   var temp = new Array(amount + 1).fill(0);
@@ -34,6 +39,7 @@ var ways = function(amount_left, newNotes) {
 }
 var numberOfWays = ways(amount_left, newNotes);
 
+// loop through the notes array to call getNofSpecificNote function
 notes.forEach(function(value, key) {
   var result = getNofSpecificNote(amount_left, value);
   if(result.quotient) {
